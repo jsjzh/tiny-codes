31a44f13e4e216c377f18af4738552f05ceaeaae
金哲豪 <jinzhehao@souche.com>
2021-05-23 15:27:27 +0800

    simple done

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-// base64 转 blob
-const dataURItoBlob = (dataURI) => {
-  // convert base64/URLEncoded data component to raw binary data held in a string
-  let byteString;
-  if (dataURI.split(",")[0].indexOf("base64") >= 0) {
-    byteString = atob(dataURI.split(",")[1]);
-  } else byteString = unescape(dataURI.split(",")[1]);
+const fs = require("fs");
 
-  // separate out the mime component
-  const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+const regDataURI = (dataURI) => {
+  const regDataURI = /^data(\:.*?)(\;.*?)?(\,.*)$/i;
 
-  // write the bytes of the string to a typed array
-  const ia = new Uint8Array(byteString.length);
-  for (let i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-  return new Blob([ia], { type: mimeString });
+  const [, _mime, _encode, _code] = regDataURI.exec(dataURI) || [];
+
+  return {
+    mime: _mime.slice(1) || null,
+    encode: _encode.slice(1) || null,
+    code: _code.slice(1) || null,
+  };
 };
 
-const base64 =
+const dataURI =
   "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
 
-(async () => {
-  const text = await dataURItoBlob(base64).text();
-  console.log(text);
-})();
+const saveDataURIImage = (dataURI, imagePath) => {
+  const { mime, encode, code } = regDataURI(dataURI);
+  var buffer = new Buffer.from(code, encode);
+  fs.createWriteStream(imagePath).write(buffer);
+};
+
+saveDataURIImage(dataURI, "image.png");
