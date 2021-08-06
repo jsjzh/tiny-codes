23bb840fe01e9d2651d749ef4991498c9bcab5b5
金哲豪 <jinzhehao@souche.com>
2021-08-06 11:01:13 +0800

    feat: 启动 jest 用作检测题目是否通过

---
+++ core/problems/__template/index.test.ts
@@ -0,0 +1,9 @@
+import { code, better } from './';
+
+it('code', () => {
+  expect(code('')).toBeFalsy();
+});
+
+it('better', () => {
+  expect(better('')).toBeFalsy();
+});
---
+++ core/problems/__template/index.ts
@@ -0,0 +1,12 @@
+/**
+ * __template
+ * 说明
+ */
+
+export const code = (str: string): boolean => {
+  return !!str;
+};
+
+export const better = (str: string): boolean => {
+  return !!str;
+};
