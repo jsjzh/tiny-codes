cf3fd0bfed828ef6c64b66abfe72b776c39c0551
jinzhehao <jinzhehao@souche.com>
2022-04-01 17:26:26 +0800

    feat: 初始化 functional-programming & rxjs demo 项目

--- core/index.ts
+++ core/index.ts
@@ -1,3 +1,10 @@
-export const sum = (a: number, b: number) => {
-  return a + b;
-};
+import _ from 'lodash/fp';
+import * as R from 'rxjs';
+
+// const arr = [1, 2, 3, 4, 5];
+
+// console.log(_.filter<number>((pre) => pre > 1)(arr));
+
+const click$ = R.fromEvent<MouseEvent>(document, 'click').pipe(R.take(3));
+
+click$.subscribe((e) => console.log(e));
